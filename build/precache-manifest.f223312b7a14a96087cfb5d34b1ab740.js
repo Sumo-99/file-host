414d0da14ee415dca2a609ca7b5a4f7d2663fe59
Sumanth <sumanthssr@gmail.com>
2022-01-18 20:20:32 +0530

    added build

---
+++ build/precache-manifest.f223312b7a14a96087cfb5d34b1ab740.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c3f3121f74540abbc689e3639a4491",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff80bfe5ae266bf42e4",
+    "url": "/static/css/main.css"
+  },
+  {
+    "revision": "fff80bfe5ae266bf42e4",
+    "url": "/static/js/main.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/main.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
